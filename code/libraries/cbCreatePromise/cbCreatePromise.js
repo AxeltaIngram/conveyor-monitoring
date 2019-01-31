b8a73b9e3ec6b15f6a595a0b90221ccbaabad4c5
AxeltaIngram <46739162+AxeltaIngram@users.noreply.github.com>
2019-01-31 18:45:32 +0530

    Asset created by AxeltaIngram

---
+++ code/libraries/cbCreatePromise/cbCreatePromise.js
@@ -0,0 +1,21 @@
+function cbCreatePromise(opts) {
+    var deferred = Q.defer();
+    if(!opts.item) {
+        log('ERROR trying to create without an item ' + opts)
+    }
+    var col = ClearBlade.Collection({ collectionName: opts.collectionName })
+    col.create(opts.item, function (err, res) {
+        
+        if (err) {
+            deferred.reject(res);
+        } else {
+            if (opts.cache) {
+                opts.item.item_id = res[0].item_id
+                opts.cache[res[0].item_id] = opts.item
+            }
+            deferred.resolve(res);
+        }
+    })
+
+    return deferred.promise;
+}
