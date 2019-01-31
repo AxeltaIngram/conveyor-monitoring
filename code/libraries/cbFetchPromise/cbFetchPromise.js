db16c49400fd46e86d5388226831301e52179b3d
AxeltaIngram <46739162+AxeltaIngram@users.noreply.github.com>
2019-01-31 18:45:33 +0530

    Asset created by AxeltaIngram

---
+++ code/libraries/cbFetchPromise/cbFetchPromise.js
@@ -0,0 +1,15 @@
+function cbFetchPromise(opts) {
+    var deferred = Q.defer();
+
+    var col = ClearBlade.Collection({ collectionName: opts.collectionName })
+    var query = opts.query || ClearBlade.Query()
+    col.fetch(query, function (err, res) {
+        if(err) {
+            deferred.reject(res);
+        } else {
+            deferred.resolve(res);
+        }
+    })
+
+    return deferred.promise;
+}
