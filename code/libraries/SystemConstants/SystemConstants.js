e9b446427ddb24641a90ea4e10e9e3eb4607f908
AxeltaIngram <46739162+AxeltaIngram@users.noreply.github.com>
2019-01-31 18:45:28 +0530

    Asset created by AxeltaIngram

---
+++ code/libraries/SystemConstants/SystemConstants.js
@@ -0,0 +1,40 @@
+// Alert Configuration Constants
+var ALERT_CONFIG_COLLECTION_NAME = "AlertConfigurations"
+
+// Alerts Constants
+var ALERTS_COLLECTION_NAME = "Alerts";
+
+// Alert Types Constants
+var ALERT_TYPES_COLLECTION_NAME = "AlertTypes";
+
+// Assets Constants
+var ASSETS_COLLECTION_NAME = "Assets";
+var ASSETS_REQUIRED_FIELDS = ["name"];
+
+// Asset Types Constants
+var ASSET_TYPES_COLLECTION_NAME = "AssetTypes";
+var ASSET_TYPES_REQUIRED_FIELDS = ["name"];
+
+// Gateways Constants
+var GATEWAYS_COLLECTION_NAME = "Gateways";
+var GATEWAYS_REQUIRED_FIELDS = ["gateway_name"];
+
+// Locations Constants
+var LOCATIONS_COLLECTION_NAME = "Locations";
+var LOCATIONS_REQUIRED_FIELDS = ["name", "lat", "long"];
+
+// Location Types Constants
+var LOCATION_TYPES_COLLECTION_NAME = "LocationTypes";
+var LOCATION_TYPES_REQUIRED_FIELDS = ["name"];
+
+// Node Data Constants
+var NODE_DATA_COLLECTION_NAME = "NodeData";
+
+// Nodes Constants
+var NODES_COLLECTION_NAME = "Nodes";
+var NODES_REQUIRED_FIELDS = ["node_name"];
+
+// Node Types Constants
+var NODE_TYPES_COLLECTION_NAME = "NodeTypes";
+var NODE_TYPES_REQUIRED_FIELDS = ["name"];
+
